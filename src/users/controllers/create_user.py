fc23994c7585d2fd67f10e0ffb571cf3d028f329
Sergio Perez <sperez4mba@gmail.com.com>
2021-12-21 11:39:21 +0100

    Added tests and remove unused import

--- src/users/controllers/create_user.py
+++ src/users/controllers/create_user.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from rest_framework import generics
 
 from users.serializers.create_user import UserCreateSerializer
---
+++ tests/users/controllers/test_create_user.py
@@ -0,0 +1,28 @@
+import json
+
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient, APITestCase
+
+
+class TestUserCreateController(APITestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+    def test_(self):
+        response = self.client.post(
+            "/users/create/",
+            data=json.dumps({"name": "Bob Doe", "email": "bob@email.com", "age": "37"}),
+            content_type="application/json",
+        )
+
+        self.assertEqual(201, response.status_code)
+        self.assertEqual(
+            {"email": "bob@email.com", "name": "Bob Doe", "age": 37},
+            response.data,
+        )
+        users = User.objects.all()
+        self.assertEqual(1, len(users))
+        self.assertEqual("bob_doe", users[0].username)
+        self.assertEqual("bob@email.com", users[0].email)
+        self.assertEqual(37, users[0].user_profile.age)
+        self.assertEqual("Bob Doe", users[0].user_profile.name)
